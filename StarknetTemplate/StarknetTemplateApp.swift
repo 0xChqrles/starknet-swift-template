5d2d180b9e8ea9ac0bccbd7ebc507b0975cee2e3
0xChqrles <clanier.dev@gmail.com>
2024-02-25 00:19:28 -0700

    first commit

---
+++ StarknetTemplate/StarknetTemplateApp.swift
@@ -0,0 +1,17 @@
+//
+//  StarknetTemplateApp.swift
+//  StarknetTemplate
+//
+//  Created by Charles Lanier on 25/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct StarknetTemplateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
